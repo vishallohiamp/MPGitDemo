dd77af1e57c14c5951a14cb1017324b8e14bdd97
gaurav7697 <gaurav.sharma@mobileprogrammingllc.com>
2017-12-29 20:47:10 +0530

    Revert "Added comment on appdelegate"
    This reverts commit b2856e74c607677e61fd64257906210d3dd94a88.

--- TestDemoSlider/TestDemoSlider/AppDelegate.swift
+++ TestDemoSlider/TestDemoSlider/AppDelegate.swift
@@ -15,11 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        /*
-         Gaurav sharma module
-         */
-        
         return true
     }
 
