b2856e74c607677e61fd64257906210d3dd94a88
gaurav7697 <gaurav.sharma@mobileprogrammingllc.com>
2017-12-29 20:44:23 +0530

    Added comment on appdelegate

--- TestDemoSlider/TestDemoSlider/AppDelegate.swift
+++ TestDemoSlider/TestDemoSlider/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        /*
+         Gaurav sharma module
+         */
+        
         return true
     }
 
